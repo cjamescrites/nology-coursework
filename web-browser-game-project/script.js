e2c50ea0f713b6e3551c2fed76e4b1fd709f1177
Cody Crites <cody91494@hotmail.com>
2022-08-03 14:50:16 -0500

    created tic tac toe

---
+++ web-browser-game-project/script.js
@@ -0,0 +1,6 @@
+// need to store buttons to be able to be clicked
+// need to target buttons and apply an X or O
+// need to switch between X and O per click to simulate turns
+// need to detect if X or O takes up three spaces in a row/column/diagonally to determine winner
+// need to detect if no spots are available and neither X nor O is taking up three to determine a tie
+// need to reset the game without refreshing
