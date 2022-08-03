bc8c6bca94a7628d1697d0f7420cf04417dbedb2
Cody Crites <cody91494@hotmail.com>
2022-08-03 16:17:57 -0500

    added more pseudo-code

--- web-browser-game-project/script.js
+++ web-browser-game-project/script.js
@@ -1,6 +1,12 @@
 // need to store buttons to be able to be clicked
+    //display hidden, clicked, and update value innerHTML
 // need to target buttons and apply an X or O
+    // update innerHTML with an X or O
 // need to switch between X and O per click to simulate turns
+    // 
 // need to detect if X or O takes up three spaces in a row/column/diagonally to determine winner
+    // if/else every possible winning solution
 // need to detect if no spots are available and neither X nor O is taking up three to determine a tie
+    // if all grid areas have a value of X/O and no winner detected
 // need to reset the game without refreshing
+    // button that sets all values back to 0
