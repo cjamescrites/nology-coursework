a0559dd30a684bd2d648c60b739c66546f4a662a
Cody Crites <cody91494@hotmail.com>
2022-08-05 16:31:21 -0500

    beginning on singplayer functionality

--- web-browser-game-project/script.js
+++ web-browser-game-project/script.js
@@ -1,18 +1,30 @@
+// ---------- Display ---------
 // ✓ need to store buttons to be able to be clicked
 // ✓ display hidden, clicked, and update value innerHTML
 // ✓ need to target buttons and apply an X or O
 // ✓ update innerHTML with an X or O
-// ✓ need to switch between X and O per click to simulate turns
 // ✓ not allow played squares to be changed
+// ✓ needs option to select between singleplayer and multiplayer
+//   once option is selected run appropriate functions & option buttons disappear
+// ---------- Multi-Player ----------
+// ✓ need to switch between X and O per click to simulate turns
+// ---------- Single-Player ----------
+//   need a basic AI that'll pick random squares that aren't selected
+// ---------- Win Conditions ----------
 // ✓ need to detect if X or O takes up three spaces in a row/column/diagonally to determine winner
 // ✓ if/else every possible winning solution
 // ✓ displays message of X/O winner
+// ---------- Tie Condition ----------
 // ✓ need to detect if no spots are available and neither X nor O is taking up three to determine a tie
 // ✓ if all grid areas have a value of X/O and no winner detected
-// need to reset the game without refreshing
-// button that sets all values back to 0
+// ---------- Reset Condition ----------
+// ✓ need to reset the game without refreshing
+// ✓ button that sets all values back to 0
+//   needs to prompt single/multi player option again
 
 //on screen buttons
+const multiPlayer = document.querySelector("#multi-player");
+const singlePlayer = document.querySelector("#single-player");
 const square = document.querySelectorAll(".square");
 const restart = document.querySelector(".restart");
 const winMessage = document.querySelector("#win-message");
@@ -32,24 +44,22 @@ const botRight = document.querySelector(".bot-right");
 let currentValue = 0;
 let playedSquares = [];
 
-// Singe Player Functions
+// Multi-Player Functions ----------------------------------------------------------
 // button assignment on click(X/O), stores Value of click
 const xClick = (xo) => {
   xo.innerHTML = "X";
   xo.value = 1;
   currentValue = 1;
-  return currentValue;
 };
 
 const oClick = (xo) => {
   xo.innerHTML = "O";
   xo.value = 2;
   currentValue = 2;
-  return currentValue;
 };
 
 // checks current value to determine whether to assign X or O, doesn't played buttons to be changed
-const displayXOSquare = (xo) => {
+const pvpDisplay = (xo) => {
   if (xo.innerHTML != "X" && xo.innerHTML != "O") {
     if (currentValue == 0 || currentValue == 2) {
       xClick(xo);
@@ -61,7 +71,15 @@ const displayXOSquare = (xo) => {
   }
 };
 
-// win conditions
+const pvp = (xo) => {
+    pvpDisplay(xo);
+}
+
+const pve = () => {
+
+}
+
+// win conditions ------------------------------------------------------------------
 // what happens when X/O wins
 const xWin = () => {
   winMessage.innerHTML = "X Wins!";
@@ -139,14 +157,16 @@ const tie = () => {
         winMessage.innerHTML = "There is a Tie!";
 }};
 
+
 square.forEach((xo) => {
   xo.addEventListener("click", () => {
-    displayXOSquare(xo);
+    pvp(xo);
     win();
     tie();
   });
 });
 
+// clear function
 const clearBoard = () => {
     currentValue = 0;
     playedSquares = [];
